74164750b10951a69f75fc06725894f4c929355d
Esteban <e.espinoza@jardinazuayo.fin.ec>
2020-08-14 10:24:45 -0500

    Onchange y restricciones

--- openacademy/models/partner.py
+++ openacademy/models/partner.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models
+from odoo import api, fields, models
+from datetime import datetime
 
 class Partner(models.Model):
-    _inherit = 'res.partner'
-
-    #instructor = fields.Boolean(default=False)
+    _inherit = "res.partner"
+    instructor = fields.Boolean(string='instructor', required=True)
     session_ids = fields.Many2many('openacademy.session', string="Attended Sessions", readonly=True)
